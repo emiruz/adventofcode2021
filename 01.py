318b3ab1fd0a5a585740f234dd8061aec492bb31
emiruz <hi@emiruz.com>
2024-03-24 16:40:21 +0100

    Day 01.

--- 01.py
+++ 01.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import sys
+from sys import stdin
 
-ls   = [int(l.strip()) for l in sys.stdin]
+ls   = [int(l.strip()) for l in stdin]
 cnt  = sum([a < b for a,b in zip(ls[:-1], ls[1:])])
 ws   = [a+b+c for a,b,c in zip(ls[:-2], ls[1:-1], ls[2:])]
 cnt2 = sum([a < b for a,b in zip(ws[:-1], ws[1:])])
