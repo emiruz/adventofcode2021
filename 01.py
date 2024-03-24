a524dc54fe0bbfea7c9386d21280c7bc902b8928
emiruz <hi@emiruz.com>
2024-03-24 14:00:48 +0100

    Day 01.

---
+++ 01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+
+ls   = [int(l.strip()) for l in sys.stdin]
+cnt  = sum([a < b for a,b in zip(ls[:-1], ls[1:])])
+ws   = [a+b+c for a,b,c in zip(ls[:-2], ls[1:-1], ls[2:])]
+cnt2 = sum([a < b for a,b in zip(ws[:-1], ws[1:])])
+
+print (f"Part 1: {cnt}, Part 2: {cnt2}")
---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of code 2021
+
+The problems are solved in *functional=style* Python.
+That is, Python pretending to be Haskell :-)
+
+All code can be run like this:
+```
+cat input.txt | ./01.py
+```
+
+## Notes
