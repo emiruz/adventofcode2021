21f4e031528d628b451709344f51d870adb6b0c5
emiruz <hi@emiruz.com>
2024-03-25 00:34:45 +0100

    Day 06.

--- 06.py
+++ 06.py
@@ -6,6 +6,6 @@ init  = list(map(int, stdin.read().strip().split(',')))
 arr   = [0,0,0,0,0,0,0,0,0]
 for x in init: arr[x] += 1
 tick  = lambda a,b,c,d,e,f,g,h,i: [b,c,d,e,f,g,a+h,i,a]
-ticks = lambda m, init: sum (reduce (lambda a,n: tick(*a), range(m), init))
+ticks = lambda m: sum (reduce (lambda a,n: tick(*a), range(m), arr))
 
-print(f"Part 1: { ticks(80, arr.copy()) }, Part 2: { ticks(256, arr) }")
+print(f"Part 1: { ticks(80) }, Part 2: { ticks(256) }")
