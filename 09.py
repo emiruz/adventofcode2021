a2b2e74d495c2d782bd25cc74ca8d6d61f30a73d
emiruz <hi@emiruz.com>
2024-03-25 12:48:50 +0100

    Day 09.

---
+++ 09.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from sys import stdin
+from math import prod
+from itertools import product
+
+ps   = [(0,-1),(0,1),(-1,0),(1,0)]
+rs   = list(map(lambda x: list(map(int, x.strip())), stdin.readlines()))
+n,m  = len(rs), len(rs[0])
+adj  = lambda i,j: ((a+i,b+j) for a,b in ps if n>a+i>=0 and m>b+j>=0)
+v    = lambda i,j: rs[i][j]
+comp = lambda i,j: all([v(i,j) < v(a,b) for a,b in adj(i,j)])
+lps  = [(i,j) for i,j in product(range(n),range(m)) if comp(i,j)]
+B_   = lambda i,j: [(a,b) for a,b in adj(i,j) if v(i,j)<v(a,b) and v(a,b)<9]
+Br   = lambda i,j: sum((Br(a,b) for a,b in B_(i,j)),[]) + [(a,b) for a,b in B_(i,j)]
+B    = lambda i,j: list(set(Br(i,j)+[(i,j)]))
+p1   = sum(1+v(i,j) for i,j in lps)
+p2   = sorted(map(lambda x: len(B(*x)), lps))
+
+print (f"Part 1: { p1 }, Part 2: { prod(p2[-3:]) }")
--- README.md
+++ README.md
@@ -11,9 +11,9 @@ I won't use anything outside of the standard library.
 
 Lines of Code (LoC):
 
-$N = 8$
+$N = 9$
 
-$\sum = 6 + 12 + 14 + 24 + 23 + 8 + 9 + 13 = 109$ LoC
+$\sum = 6 + 12 + 14 + 24 + 23 + 8 + 9 + 13 + 15= 124$ LoC
 
 $\mu = \frac{\sum}{N} \sim 14$ LoC
 
