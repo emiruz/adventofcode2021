f53ea87406d70843d3205de8fee4374c6f0d62c3
emiruz <hi@emiruz.com>
2024-03-29 18:20:47 +0100

    Day 02, Scala.

---
+++ 02.scala
@@ -0,0 +1,13 @@
+#!/usr/bin/env scala
+import scala.io.Source.stdin
+
+@main def aoc(): Unit = 
+  val ns = stdin.getLines().map(_.split(' ')).map(x=>(x(0).head, x(1).toInt)).toList
+  val f = (k:Char,v:Int) => k match { case 'f'=>(v,0) case 'u'=>(0,-v) case 'd'=>(0,v) }
+  val p1 = ns.map(f.tupled).reduce((a,b)=>(a._1+b._1, a._2+b._2)).toList
+  val p2 = ns.foldLeft((0,0,0))((a,b)=>b._1 match {
+    case 'f'=>(a._1,       a._2+b._2, a._3+a._1*b._2)
+    case 'u'=>(-b._2+a._1, a._2,      a._3)
+    case 'd'=>(b._2+a._1,  a._2,      a._3) }).toList
+
+  println(s"Part 1: ${ p1.product }, Part 2: ${ p2.tail.product }")
